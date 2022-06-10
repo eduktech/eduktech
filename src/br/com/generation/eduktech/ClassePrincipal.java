ca3ac7c8658f44c51be3c78cc0bb7cf2070eabec
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-06-10 13:52:22 -0300

    criando classe principal

---
+++ src/br/com/generation/eduktech/ClassePrincipal.java
@@ -0,0 +1,11 @@
+package br.com.generation.eduktech;
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Seja bem vindes!");
+
+	}
+
+}
