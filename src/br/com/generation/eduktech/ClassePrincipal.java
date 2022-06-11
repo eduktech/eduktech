03cf6bd05b0afcc22196c8340682468a170b1750
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-06-11 19:31:14 -0300

    adicionando condicoes if na classe principal

--- src/br/com/generation/eduktech/ClassePrincipal.java
+++ src/br/com/generation/eduktech/ClassePrincipal.java
@@ -1,11 +1,42 @@
 package br.com.generation.eduktech;
 
+import java.util.Scanner;
+
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Seja bem vindes!");
+		//inicio
+		System.out.println("===Inicio do programa===");
+		Scanner in = new Scanner(System.in);
+		
+		int resposta1, idade;
+		
+		System.out.println("Você tem interesse na carreira Tech?");
+		System.out.println("Digite 1 para sim e 2 para não.");
+		System.out.println("1 - Sim | 2 - Não");
+		resposta1 = in.nextInt();
+		if(resposta1 == 1) {
+			//classe com outras perguntas
+			System.out.println("Qual sua idade? ");
+			idade = in.nextInt();
+			if(idade >= 6 && idade <= 12) {
+				//categoria-infantil
+			}
+			if(idade > 12 && idade <= 17) {
+				//Adolescentes();
+			}
+			if(idade > 17 && idade <= 100) {
+				//adultos
+			}
+			
+		}else {
+			//mensagem de incentivo ou despedida
+		}
 
 	}
 
 }
+
+
