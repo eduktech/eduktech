3b531d732209d020f102cfc175d28cc0c5e5ab06
Leonardo luis rampazi <105321172+Leonardo-Rampazi@users.noreply.github.com>
2022-06-11 13:59:24 -0300

    colocando um pouco do meu mobile

---
+++ src/br/com/generation/eduktech/mobile.java
@@ -0,0 +1,13 @@
+package br.com.generation.eduktech;
+import java.util.Scanner;
+public class mobile {
+	Scanner leia = new Scanner (System.in);
+String nome;
+void tiposDeMobile() {
+	
+System.out.println("-----> Sejam Bem vindes a pagina Mobile <-----");
+System.out.println("Digite qual tipo de curso você vai acessar de mobile: 1 para: Android Studio 2 para: Phonegap 3 para: Appcelerator");
+
+}
+}
+
